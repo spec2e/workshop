2570423aa207f336466eca1e76f59071f4611369
zapp <obe2lix>
2014-12-17 21:38:22 +0100

    fixed naming stuff

--- workshop/details/DetailsCtrl.js
+++ workshop/details/DetailsCtrl.js
@@ -1,3 +1,22 @@
-/**
- * Created by zapp on 17/12/14.
- */
+angular.module("workshop.details")
+
+    .controller("DetailsCtrl", function ($stateParams, Employees) {
+
+        var details = this;
+
+        init();
+
+        function init() {
+            details.person = getDetails($stateParams.id);
+        }
+
+        function getDetails(id) {
+            var ret = null;
+            angular.forEach(Employees.persons, function(person) {
+                if(person.id === id) {
+                    ret = person;
+                }
+            });
+            return ret;
+        }
+    });
--- workshop/details/details.js
+++ workshop/details/details.js
@@ -9,26 +9,7 @@ angular.module("workshop.details", [])
                 templateUrl: "details/details.html"
             })
 
-    })
-
-    .controller("DetailsCtrl", function ($stateParams, Employees) {
-
-        var details = this;
-
-        init();
-
-        function init() {
-            details.person = getDetails($stateParams.id);
-        }
-
-        function getDetails(id) {
-            var ret = null;
-            angular.forEach(Employees.persons, function(person) {
-                if(person.id === id) {
-                    ret = person;
-                }
-            });
-            return ret;
-        }
     });
+
+
 
